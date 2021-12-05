eff9a85bfe311ee1f270eb41ab85c522b5fe9530
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-12-05 16:28:55 +0530

    added more questions

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector(".quiz-form");
 const submitBtn = document.querySelector("#submit-btn");
 const output = document.querySelector("#output");
 
-const correctAnswers = ["90°", "right angled"];
+const correctAnswers = ["90°", "right angled", "90°", "Hypotenuse", "Polygon"];
 
 function calcScore() {
   let score = 0;
