5cb10559f04ca4228b503dc8ec5f977c8dd5725e
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-12-05 17:10:57 +0530

    final update of hypotenuse project

--- hypotenuse.js
+++ hypotenuse.js
@@ -4,7 +4,10 @@ const calcBtn = document.querySelector("#calc-btn");
 const output = document.querySelector("#output");
 
 function calcHypotenuse() {
-  console.log("clicked");
+  const sum = a.value * a.value + b.value * b.value;
+  let hypotenuse = (Math.sqrt(sum));
+  hypotenuse = hypotenuse.toFixed(1);
+  output.innerText = "The hypotenuse of the triangle is " + hypotenuse + " cm";
 }
 
 calcBtn.addEventListener("click", calcHypotenuse);
