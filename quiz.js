97f16fe28374e199f0a7749fe38a0dcfbc4409e5
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-12-05 16:05:03 +0530

    update quiz.js
    wiring of submit button

--- quiz.js
+++ quiz.js
@@ -3,3 +3,9 @@ const submitBtn = document.querySelector("#submit-btn");
 const output = document.querySelector("#output");
 
 const correctAnswers = ['90°', 'right angled'];
+
+function clicked(){
+    console.log("clicked");
+}
+
+submitBtn.addEventListener("click", clicked);
