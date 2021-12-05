3cf7ccd6f3570d918fc72f55ea92f943c000f2b8
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-12-05 16:01:17 +0530

    create quiz.js

---
+++ quiz.js
@@ -0,0 +1,5 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitBtn = document.querySelector("#submit-btn");
+const output = document.querySelector("#output");
+
+const correctAnswers = ['90°', 'right angled'];
